2f691d0031bc901391b1112d53a636b9b5d29fbe
Cosmin Vasile <vcosmin961@gmail.com>
2023-12-07 20:55:25 +0200

    Finished computer choice function. Randomly choosing.

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+  const arr = ['rock', 'paper', 'scissors']
+
+  console.log(arr[Math.floor(Math.random() * 3)])
+}
+getComputerChoice()
+
+function playerSelection() {
+
+}
+
+function playRound(getComputerChoice, playerSelection) {
+  
+}
