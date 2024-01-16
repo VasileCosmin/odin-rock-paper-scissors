1f1cfbe89de9de665995359cb4c58981ea6f17d1
Cosmin Vasile <vcosmin961@gmail.com>
2024-01-16 15:49:26 +0200

    Built player choice function. It lets you choose between rock, paper or scissors

--- script.js
+++ script.js
@@ -6,8 +6,14 @@ function getComputerChoice() {
 }
 
 function playerSelection() {
-
+  let choice = prompt('Choose between rock, paper and scissors: ')
+  while (choice.toLowerCase() != 'rock' && choice.toLowerCase() != 'paper' && choice.toLowerCase() != 'scissors') {
+    choice = prompt('Choose between rock, paper and scissors: ')
+  }
+  return `Your choice is ${choice}`
 }
+
+playerSelection()
 
 function playRound(getComputerChoice, playerSelection) {
   
